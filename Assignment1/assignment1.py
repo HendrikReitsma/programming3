5e8e8288ad274d6021f4648512aefb993a19e7aa
Hendrik Reitsma <hendrikreitsma@hotmail.com>
2022-05-12 14:42:02 +0200

    finished assignment1

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -4,6 +4,19 @@ import multiprocessing as mp
 Entrez.api_key = "cc90e8524a1e6db189cc428e8ddb8a862208"
 Entrez.email = 'h.reitsma@st.hanze.nl'
 
+def get_citation_ids(pmid):
+    """
+    Input: pubmed id
+    :return: references
+    """
+    results = Entrez.read(Entrez.elink(dbfrom="pubmed",
+                                   db="pmc",
+                                   LinkName="pubmed_pmc_refs",
+                                   id=pmid,
+                                   api_key='cc90e8524a1e6db189cc428e8ddb8a862208'))
+    references = [f'{link["Id"]}' for link in results[0]["LinkSetDb"][0]["Link"]]
+    return references
+
 def pubmed_id_to_xml(pubmed_id):
     '''
     parameter: pubmed_id
@@ -16,21 +29,25 @@ def pubmed_id_to_xml(pubmed_id):
     with open('output/'+str(pubmed_id)+'.xml', 'w') as f:
         for line in records:
             f.write(str(line))
-    return
+    return True
 
 def main():
     pmid = sys.argv[1]
     # pmid = '19304878'
     cpus = mp.cpu_count()
-    record = Entrez.read(Entrez.elink(dbfrom="pubmed", id=pmid))
-
-    related_ids = []
-    for i in range(1,11):
-        related_id = record[0]["LinkSetDb"][5]["Link"][i]['Id']
-        related_ids.append(int(related_id))
-    print(related_ids)
+    # record = Entrez.read(Entrez.elink(dbfrom="pubmed", id=pmid))
+    ## Get the related ID's:
+    # related_ids = []
+    # results = Entrez.read(Entrez.elink(dbfrom="pubmed", id=pmid))
+    # related_ids = [link["Id"] for link in results[0]["LinkSetDb"][5]["Link"]]
+    # related_ids = related_ids[:10]
+    # for i in range(1,11):
+    #     related_id = record[0]["LinkSetDb"][5]["Link"][i]['Id']
+    #     related_ids.append(int(related_id))
+    # print(related_ids)
+    references = get_citation_ids(pmid)
     with mp.Pool(cpus) as pool:
-        pool.map(pubmed_id_to_xml, related_ids)
+        pool.map(pubmed_id_to_xml, references[:10])
 
 if __name__ == '__main__':
     main()
