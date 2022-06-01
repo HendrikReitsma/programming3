e8d765b1b5b618857e7389284721b86f70d45f52
HendrikReitsma <h.reitsma@st.hanze.nl>
2022-06-01 12:19:52 +0200

    finished bash and python script

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -1,6 +1,5 @@
  #!/usr/bin/env python
 import matplotlib.pyplot as plt
-import sys
 import pandas as pd
 
 def read_timings_txt():
@@ -16,11 +15,16 @@ def draw_graph(df):
     draws graph
     """
     plt.plot(df.iloc[:,0], df.iloc[:,1])
-    plt.savefig('timings.png')
+    plt.title('Core / timing')
+    plt.xlabel('cores used')
+    plt.ylabel('elapsed time')
+    plt.savefig('output/timings.png')
     return
 
 def main():
     df = read_timings_txt()
+    df = df.T.reset_index(drop=False).T
+    df = df.astype(float)
     draw_graph(df)
 
 if __name__ == "__main__":
